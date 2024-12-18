9187bb8cfdc9ff9a0c12041a13738b6f9f43020d
paviishere <pavithranmacse@gmail.com>
2024-12-19 00:04:16 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "pavinet"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "pavi"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "2003"
 )
 
 var ID ids.ID
